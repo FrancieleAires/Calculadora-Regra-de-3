dba71646571b83943c987661eda18a83557784e0
FrancieleAires <f.aires2016@gmail.com>
2023-03-03 21:20:51 +0900

    update

--- www/components/main.js
+++ www/components/main.js
@@ -1,5 +1,9 @@
 var resultado = ""
 var resultado1 = ""
+var resultado2 = ""
+var resultado3 = ""
+var resultado4 = ""
+var resultado5 = ""
 function calcularregrade3 () {
     var valor1, valor2, valor3
 
@@ -7,12 +11,22 @@ function calcularregrade3 () {
     valor2 = document.getElementById('valor2').value
     valor3 = document.getElementById('valor3').value
 
+    resultado5 = resultado4;
+    resultado4 = resultado3;
+    resultado3 = resultado2;
+    resultado2 = resultado1;
     resultado1 = resultado;
+    
 
     resultado = (valor2 * valor3) / valor1
 
     document.getElementById('resultado').value = resultado 
-      document.getElementById('resultado1').textContent = resultado1
+    document.getElementById('resultado1').textContent = resultado
+    document.getElementById('resultado2').textContent = resultado1
+    document.getElementById('resultado3').textContent = resultado2
+    document.getElementById('resultado4').textContent = resultado3
+    document.getElementById('resultado5').textContent = resultado4
+    
    
 
 }
